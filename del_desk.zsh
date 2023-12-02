f4422d424b9c7bfcea96ac4bd16cd70f4053cebf
Adrienucl <adrien.payen@student.uclouvain.be>
2023-12-02 13:43:27 +0100

    desktop delete

---
+++ del_desk.zsh
@@ -0,0 +1,4 @@
+# Delete your desktop
+defaults write com.apple.finder CreateDesktop false; killall Finder (supprimer les docs sur le desktop)
+# or 
+defaults write com.apple.finder CreateDesktop true; killall Finder (afficher les docs sur le desktop)
